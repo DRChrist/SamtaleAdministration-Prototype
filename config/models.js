d8acc7123a4b72cb16a51a478b5bb2f3f6381f92
unknown <dennis_christensen87@hotmail.com>
2017-11-28 22:48:42 +0100

    Log and added mysql connection to help create er-diagram

--- config/models.js
+++ config/models.js
@@ -12,12 +12,13 @@
 module.exports.models = {
 
   /***************************************************************************
-  *                                                                          *
-  * Your app's default connection. i.e. the name of one of your app's        *
-  * connections (see `config/connections.js`)                                *
-  *                                                                          *
-  ***************************************************************************/
-  connection: 'localDiskDb',
+   *                                                                          *
+   * Your app's default connection. i.e. the name of one of your app's        *
+   * connections (see `config/connections.js`)                                *
+   *                                                                          *
+   ***************************************************************************/
+  // connection: 'localDiskDb',
+  connection: 'someMysqlServer',
 
   /***************************************************************************
   *                                                                          *
